ad9dcb1c826a9bd34985cb798e3bb86ebc73244d
Weiyi Xu <johnsonxwy@gmail.com>
2020-10-09 22:41:29 -0400

    finish task2-a mapper

---
+++ task2a-map.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+f = open('/Users/johnson/Documents/2019 - 2021 NYU/03_Term/2020 - 2021 Fall/CS-GY 6513 – Big Data/03_Assignments/Homework 01 Data Analysis Using Map Reduce/hw1data/test script/task1outputsample.csv')
+
+# input comes from STDIN
+for i in range(50):
+    # remove leading and trailing whitespace
+    line = f.readline()
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # get key & value
+    key, values = line.split('\t')
+    # get list of values
+    values = values.split(',')
+    print '%s\t%s' % (values[11], 1)
