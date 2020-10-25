ee70307b3bd728fbeb2feb3740caa5a53237cd1e
Weiyi Xu <johnsonxwy@gmail.com>
2020-10-25 14:35:13 -0400

    finish task3b

---
+++ Homework 02 Spark SQL/task3b.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import sys
+from pyspark import SparkContext
+from csv import reader
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: wordcount <file 1>", file=sys.stderr)
+        exit(-1)
+
+    sc = SparkContext()
+    more_than_one = sc.textFile(sys.argv[1], 1) \
+                      .mapPartitions(lambda x: reader(x)) \
+                      .map(lambda x: ((x[0], x[3]), 1)) \
+                      .reduceByKey(lambda x, y: x + y) \
+                      .filter(lambda x: x[1] > 1) \
+                      .map(lambda x: ','.join([x[0][0], x[0][1]]))
+
+    more_than_one.saveAsTextFile('task3b.out')
+
+    sc.stop()
